bba1f0900e364057c4d523e75d2d27f84506cb3d
Andres <andresm09872@gmail.com>
2026-05-09 14:41:09 -0600

    feat(dashboard-page): crear componente DashboardPage con estructura básica

---
+++ frontend/src/pages/DashboardPage.jsx
@@ -0,0 +1,17 @@
+function DashboardPage() {
+
+    return (
+
+        <div>
+
+            <h1>Dashboard</h1>
+
+            <a href="/exercise">
+                Ir a ejercicios
+            </a>
+
+        </div>
+    );
+}
+
+export default DashboardPage;
