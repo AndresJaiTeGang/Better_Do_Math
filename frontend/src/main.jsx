9f56b693ba51e3e559d368a94f8f23709120637d
Andres <andresm09872@gmail.com>
2026-05-09 15:27:24 -0600

    Agregar frontend inicial con Vite + React

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,26 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './styles/global.css'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-import App from './App'
-
-import { BrowserRouter }
-from 'react-router-dom'
-
-import { AuthProvider }
-from './context/AuthContext'
-
-ReactDOM.createRoot(
-    document.getElementById('root')
-).render(
-
-    <BrowserRouter>
-
-        <AuthProvider>
-
-            <App />
-
-        </AuthProvider>
-
-    </BrowserRouter>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
