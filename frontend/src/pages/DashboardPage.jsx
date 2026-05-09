015cd0299775828c8eb21ee5910b7a9f40c84f9f
Andres <andresm09872@gmail.com>
2026-05-09 15:05:00 -0600

    update DashboardPage.jsx

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,14 +1,66 @@
+import { useEffect,
+         useState }
+from "react";
+
+import Navbar
+from "../components/Navbar";
+
+import DashboardChart
+from "../components/DashboardChart";
+
+import * as dashboardService
+from "../services/dashboardService";
+
 function DashboardPage() {
+
+    const [stats, setStats] =
+        useState(null);
+
+    useEffect(() => {
+
+        const load = async () => {
+
+            const data =
+                await dashboardService
+                    .getStats();
+
+            setStats(data);
+        };
+
+        load();
+
+    }, []);
+
+    if(!stats)
+        return <p>Cargando...</p>;
 
     return (
 
         <div>
 
+            <Navbar />
+
             <h1>Dashboard</h1>
 
-            <a href="/exercise">
-                Ir a ejercicios
-            </a>
+            <h3>
+                Aciertos:
+                {stats.correctos}
+            </h3>
+
+            <h3>
+                Incorrectos:
+                {stats.incorrectos}
+            </h3>
+
+            <h3>
+                Porcentaje:
+                {stats.porcentaje.toFixed(2)}%
+            </h3>
+
+            <DashboardChart
+                correctos={stats.correctos}
+                incorrectos={stats.incorrectos}
+            />
 
         </div>
     );
