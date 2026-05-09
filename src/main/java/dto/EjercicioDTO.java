e5e971902101106041f9e7600dd901315e06bbe7
Andres <andresm09872@gmail.com>
2026-05-09 09:53:00 -0600

    creación de la lógica matemática de ejercicios

---
+++ src/main/java/dto/EjercicioDTO.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dto;
+
+public class EjercicioDTO {
+
+    public int numeroA;
+    public int numeroB;
+
+    public EjercicioDTO(int a, int b) {
+
+        this.numeroA = a;
+        this.numeroB = b;
+    }
+}
