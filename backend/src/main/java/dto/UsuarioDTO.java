8d49688257833c2906943565e3952eebecbc8a93
Andres <andresm09872@gmail.com>
2026-05-10 11:58:21 -0600

    Add CRUD backend users

---
+++ backend/src/main/java/dto/UsuarioDTO.java
@@ -0,0 +1,18 @@
+package dto;
+
+public class UsuarioDTO {
+
+    public Long id;
+    public String username;
+    public String rol;
+
+    public UsuarioDTO(
+            Long id,
+            String username,
+            String rol) {
+
+        this.id = id;
+        this.username = username;
+        this.rol = rol;
+    }
+}
